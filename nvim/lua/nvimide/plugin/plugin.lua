cc5999c0b08ca730d3999eb34a6c1e252b499709
shaowu <breeze101792@gmail.com>
2025-04-04 11:59:06 +0800

    Plugin: Add LLM impl for nvim
    [Description]
    Plugin: Add LLM impl for nvim
    
    [Self-Test]
    - Build succeeded.
    - Unit tests for new endpoints passed.

--- nvim/lua/nvimide/plugin/plugin.lua
+++ nvim/lua/nvimide/plugin/plugin.lua
@@ -108,76 +108,6 @@ local lspui = function(opts)
         float={border=_border}
     }
 end
-----------------------------------------------------------------
-----    Plugin
-----------------------------------------------------------------
-local plug_avante = function()
-    return {
-        "yetone/avante.nvim",
-        event = "VeryLazy",
-        version = false, -- Never set this value to "*"! Never!
-        opts = {
-            -- add any opts here
-            -- for example
-            --[[
-            provider = "openai",
-            openai = {
-            endpoint = "https://api.openai.com/v1",
-            model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-            timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-            temperature = 0,
-            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-            },
-            --]]
-            provider = "ollama",
-            ollama = {
-                model = "qwen2.5-coder",
-            }
-        },
-        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = "make",
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "stevearc/dressing.nvim",
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            --- The below dependencies are optional,
-            "echasnovski/mini.pick", -- for file_selector provider mini.pick
-            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-            "ibhagwan/fzf-lua", -- for file_selector provider fzf
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua", -- for providers='copilot'
-            {
-                -- support for image pasting
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    -- recommended settings
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                        -- required for Windows users
-                        use_absolute_path = true,
-                    },
-                },
-            },
-            {
-                -- Make sure to set this up properly if you have lazy=true
-                'MeanderingProgrammer/render-markdown.nvim',
-                opts = {
-                    file_types = { "markdown", "Avante" },
-                },
-                ft = { "markdown", "Avante" },
-            },
-        },
-    }
-end
 
 ----------------------------------------------------------------
 ----    Main
@@ -303,37 +233,13 @@ function M.init()
             vim.api.nvim_exec("GitGutterEnable", false)
         end},
 
-        -- LLM config
-        {"gen.nvim", dir = vim_plugin_path .. "gen.nvim", lazy = true, cmd = "Gen",
-        opts = {
-            model = vim.g.IDE_CFG_LLM_MODEL, -- The default model to use.
-            quit_map = "q", -- set keymap for close the response window
-            retry_map = "<c-r>", -- set keymap to re-send the current prompt
-            accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-            host = vim.g.IDE_CFG_LLM_SERVER, -- The host running the Ollama service.
-            port = vim.g.IDE_CFG_LLM_SERVER_PORT, -- The port on which the Ollama service is listening.
-            display_mode = "horizontal-split", -- The display mode. Can be "float" or "split" or "horizontal-split".
-            show_prompt = true, -- Shows the prompt submitted to Ollama.
-            show_model = true, -- Displays which model you are using at the beginning of your chat session.
-            no_auto_close = false, -- Never closes the window automatically.
-            hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
-            init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
-            -- Function to initialize Ollama
-            command = function(options)
-                local body = {model = options.model, stream = true}
-                return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
-            end,
-            -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-            -- This can also be a command string.
-            -- The executed command must return a JSON object with { response, context }
-            -- (context property is optional).
-            -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-            debug = false -- Prints errors and the command which is run.
-        }},
 
         -- Others
         -- { "srcexpl"         , dir = vim_plugin_path .. "srcexpl"         , lazy = true , cmd = {"SrcExplRefresh"        , "SrcExplToggle"} } ,
     }
+    local llm = require('nvimide.plugin.llm')
+
+    table.insert(lazy_plugin,llm.get_impl(vim.g.IDE_CFG_LLM_IMPL))
 
     local lazy_opts = {
         defaults = {
@@ -346,7 +252,7 @@ function M.init()
             -- version = "*", -- try installing the latest stable version for plugins that support semver
         },
         -- install = { colorscheme = { "tokyonight", "habamax" } },
-        checker = { enabled = true }, -- automatically check for plugin updates
+        checker = { enabled = false }, -- automatically check for plugin updates
         performance = {
             cache = {
                 enabled = true,
